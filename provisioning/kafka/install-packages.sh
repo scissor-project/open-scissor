8ce7f26374868f41a71a67c844862d0c56ec1ef1
Marco Ferrari <ferrari.marco@gmail.com>
2017-10-13 15:52:04 +0200

    Do not use unnecessary sudos

--- provisioning/kafka/install-packages.sh
+++ provisioning/kafka/install-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
 
 # Set values with debconf-set-selections to avoid questions during package installation
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 
 apt-get install -y \
   default-jre \
