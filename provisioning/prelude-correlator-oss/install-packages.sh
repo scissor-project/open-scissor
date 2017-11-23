5f778ad9f8d92b66a5b7fdbc26b796677d58019a
Marco Ferrari <ferrari.marco@gmail.com>
2017-11-23 11:00:35 +0100

    Cleanup of provisioning scripts

--- provisioning/prelude-correlator-oss/install-packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -exe
-
-yum install -y \
-  prelude-correlator \
-  prelude-correlator-selinux \
-  prelude-tools
--- provisioning/prelude-correlator-oss/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xeuo pipefail
-IFS=$'\\n\t'
-
-# don't start it yet, it is not configured
-systemctl enable prelude-correlator
