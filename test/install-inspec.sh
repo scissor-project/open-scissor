f7f6f7eb351492084f4f85b25085f15092d5fee3
Marco Ferrari <ferrari.marco@gmail.com>
2017-11-30 13:22:11 +0100

    Install inspec gem

--- test/install-inspec.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-if which inspec >/dev/null 2>&1 ; then
-  echo "inspec is already installed"
-else
-  wget -O /tmp/inspec.deb https://packages.chef.io/files/stable/inspec/1.46.2/ubuntu/16.04/inspec_1.46.2-1_amd64.deb
-  dpkg -i /tmp/inspec.deb
-fi
