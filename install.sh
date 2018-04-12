33a09db98b478351ba3bcac91f091bf98ee83c32
Marco Ferrari <ferrari.marco@gmail.com>
2018-04-12 11:15:44 +0000

    Refactor install.sh
    1) Use apt-get instead of apt: apt interface is not stable by design
    2) Do not use sudo in the script: instead run the whole script as sudo
    3) Upgrade InSpec to 2.1.30
    4) Upgrade Docker Compose to 1.21.0

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 echo -e "\\n##### Install libs #####\\n"
 
-sudo apt install -y ruby-dev
-sudo apt install -y gcc
-sudo apt install -y make
+apt-get install -y ruby-dev
+apt-get install -y gcc
+apt-get install -y make
 
 echo -e "\\n##### Install Docker #####\\n"
 
 curl -sSL https://get.docker.com | sh
-sudo usermod -aG docker "$USER"
+usermod -aG docker "$USER"
 
-echo -e "\\n##### Install inspec -v 1.49.2 #####\\n"
+echo -e "\\n##### Install inspec #####\\n"
 
-sudo gem install inspec -v 1.49.2
+gem install inspec -v 2.1.30
 
 echo -e "\\n##### Install docker-compose #####\\n"
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
