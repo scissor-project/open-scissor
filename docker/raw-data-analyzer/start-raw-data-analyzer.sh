75a8e4df638edb4aaeb1bc220ab365ff59a77ce5
Alessandro Fazio <agil3b3ast@hotmail.it>
2018-04-23 13:25:29 +0200

    Fixed wrong startup conf

--- docker/raw-data-analyzer/start-raw-data-analyzer.sh
+++ docker/raw-data-analyzer/start-raw-data-analyzer.sh
@@ -7,7 +7,7 @@ while ! ncat -vvz kafka 9092; do echo "Wating 10 secs..."; sleep 10; done
 # workaround for tail -F saying: "has been replaced with a remote file. giving up on this name"
 truncate -s0 "$SCISSOR_LOG_DIR"/raw-data-analyzer-output.log;
 
-sh -c "converter /etc/envMonAnalyzer.conf" > "$SCISSOR_LOG_DIR"/raw-data-analyzer-output.log 2>&1 &
+sh -c "converter /etc/converter.conf" > "$SCISSOR_LOG_DIR"/raw-data-analyzer-output.log 2>&1 &
 
 tail -F \
   "$SCISSOR_LOG_DIR"/raw-data-analyzer-output.log
